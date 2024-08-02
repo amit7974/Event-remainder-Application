77eaad4347f6cc91be25c313d7657bf250dbe28b
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2024-08-02 09:57:52 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Header = (props) => {
+  return (
+    <View style={{marginLeft:15}}>
+      <Text style={{fontWeight:'bold',fontSize:28}}>Header</Text>
+    </View>
+  )
+}
+
+export default Header
